321524a86775d70310abeea31f2f3e1fb1536c29
Ricardo Rodriguez <m3ssiak@gmail.com>
2019-09-15 14:57:27 -0500

    seeds created to fill burger_db

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,5 @@
 -- seeds.sql
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('The Classic', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Ranch', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Jalapeno Burger', false);
